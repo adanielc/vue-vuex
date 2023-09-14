46364d3c1e89b898e5efb4d3f52f8487183e9ed8
Scoresby <andresdaniel@gmail.com>
2023-09-14 21:19:25 +0200

    axios example

---
+++ src/components/ClassesList.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-row v-for="item in classes" :key="item.index">
+      <v-card
+        @click="getClass(item.url)"
+        elevation="2"
+        class="mx-auto my-12"
+        width="374"
+      >
+        <v-card-title class="mx-auto my-12">
+          {{ item.name }}
+        </v-card-title>
+        <v-card-text>
+          <v-row> </v-row>
+        </v-card-text>
+      </v-card>
+    </v-row>
+    <v-row>
+      <v-btn @click="get">Get</v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "ClassesList",
+
+  data: () => ({
+    resp: {},
+    classes: [],
+  }),
+  computed: {},
+  methods: {
+    get() {
+      axios({
+        method: "get",
+        url: "https://www.dnd5eapi.co/api/classes ",
+      }).then((response) => {
+        console.log("response result: " + response.data.results);
+        this.classes = response.data.results;
+        // this.classes = response.data.results.map((e) => e);
+      });
+    },
+    getClass(suffix) {
+      let url = "https://www.dnd5eapi.co" + suffix;
+      console.log("url: " + url);
+      axios({
+        method: "get",
+        url: url,
+      }).then((response) => {
+        console.log("response result: " + response.data);
+        this.resp = response.data;
+      });
+    },
+  },
+  mounted() {},
+};
+</script>
---
+++ src/views/ClassesView.vue
@@ -0,0 +1,15 @@
+<template>
+  <classes-list />
+</template>
+
+<script>
+import ClassesList from "@/components/ClassesList.vue";
+
+export default {
+  name: "ClassesView",
+
+  components: {
+    ClassesList,
+  },
+};
+</script>
